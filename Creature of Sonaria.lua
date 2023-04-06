68c1ea5ee164eb348383947876754a5d1e5f7753
Mangnex <127064046+Mangnex@users.noreply.github.com>
2023-04-06 13:18:13 -0500

    Update Creature of Sonaria.lua

--- Creature of Sonaria.lua
+++ Creature of Sonaria.lua
@@ -1,1 +1,1 @@
-error("The script is being updated the link is currently down please wait until the update!")
+game:service('Players').LocalPlayer:Kick('The script is being updated the link is currently down please wait until the update')
